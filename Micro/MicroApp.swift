9e22540567c4c8ea64ea4f6bd19ba770da011333
hajarAlshehri22 <147072959+hajarAlshehri22@users.noreply.github.com>
2024-04-18 15:04:30 +0300

    Initial Commit

---
+++ Micro/MicroApp.swift
@@ -0,0 +1,17 @@
+//
+//  MicroApp.swift
+//  Micro
+//
+//  Created by Hajar Alshehri on 09/10/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct MicroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
