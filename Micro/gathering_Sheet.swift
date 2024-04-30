2d15724a1267ddc51f61ca96819ed14306f59339
hajarAlshehri22 <147072959+hajarAlshehri22@users.noreply.github.com>
2024-04-30 15:25:27 +0300

    push map page

---
+++ Micro/gathering_Sheet.swift
@@ -0,0 +1,18 @@
+//
+//  gathering_Sheet.swift
+//  Micro
+//
+//  Created by Hajar Alshehri on 19/10/1445 AH.
+//
+
+import SwiftUI
+
+struct gathering_Sheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    gathering_Sheet()
+}
