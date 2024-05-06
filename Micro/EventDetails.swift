4462600b9fe459ebab753b0e71460ef1800a82b5
Mayaner <mem.kpope@gmail.com>
2024-05-06 10:48:04 +0300

    Event fetching plan

---
+++ Micro/EventDetails.swift
@@ -0,0 +1,8 @@
+//
+//  EventDetails.swift
+//  Micro
+//
+//  Created by Mayasem Muner on 5/1/24.
+//
+
+import Foundation
---
+++ Micro/PickGatheringDay.swift
@@ -0,0 +1,18 @@
+//
+//  PickGatheringDay.swift
+//  Micro
+//
+//  Created by Mayasem Muner on 5/1/24.
+//
+
+import SwiftUI
+
+struct PickGatheringDay: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PickGatheringDay()
+}
