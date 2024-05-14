abca24e0290fd129336eb21562ebfd5075000763
hajarAlshehri22 <147072959+hajarAlshehri22@users.noreply.github.com>
2024-05-14 21:31:45 +0300

    modify sign in function to check if it new or exisit user, and link tab bar to the pages

--- Micro/view/theTabBar.swift
+++ Micro/view/theTabBar.swift
@@ -15,43 +15,60 @@ struct TabBar: View {
     @ObservedObject var tabItems = TabItems.shared
     
     @State private var circleSize: CGFloat = 65 // Adjust the size of the circles here
-    @State private var iconeSize: CGFloat = 25 // Adjust the size of the icons here
-    
+    @State private var iconSize: CGFloat = 25 // Adjust the size of the icons here
+    @State private var selectedView: AnyView = AnyView(Entertainment())
+
     var body: some View {
-        ZStack {
-            Color.white.edgesIgnoringSafeArea(.all)
-            VStack {
-                Spacer()
-                ZStack {
-                    Bar(tab: tabItems.selectedTabIndex)
-                        .foregroundColor(color)
-                        .frame(width: UIScreen.main.bounds.width, height: 88)
-                    HStack(spacing: (UIScreen.main.bounds.width - (CGFloat(tabItems.items.count + 1) * self.circleSize)) / (CGFloat(tabItems.items.count) + 1)) {
-                        ForEach(0..<tabItems.items.count, id: \.self) { i in
-                            ZStack {
-                                Circle()
-                                    .frame(width: self.circleSize, height: self.circleSize)
-                                    .foregroundColor(color)
-                                    .onTapGesture {
-                                        self.tabItems.select(i)
-                                        
-                                        // Code to change tab screen can go here...
-                                    }
-                                Image(systemName: self.tabItems.items[i].imageName)
-                                    .resizable()
-                                    .foregroundColor(Color(self.darkBackground))
-                                    .frame(width: self.iconeSize, height: self.iconeSize)
-                                    .opacity(self.tabItems.items[i].opacity)
+            ZStack {
+                Color.white.edgesIgnoringSafeArea(.all)
+                VStack {
+                    selectedView // This displays the selected view
+                        .transition(.slide) // Add a transition if desired
+                    Spacer()
+                    ZStack {
+                        Bar(tab: tabItems.selectedTabIndex)
+                            .foregroundColor(color)
+                            .frame(width: UIScreen.main.bounds.width, height: 88)
+                        HStack(spacing: (UIScreen.main.bounds.width - (CGFloat(tabItems.items.count + 1) * self.circleSize)) / (CGFloat(tabItems.items.count) + 1)) {
+                            ForEach(Array(tabItems.items.enumerated()), id: \.element.id) { index, item in
+                                ZStack {
+                                    Circle()
+                                        .frame(width: self.circleSize, height: self.circleSize)
+                                        .foregroundColor(color)
+                                        .onTapGesture {
+                                            self.tabItems.select(index)
+                                            self.updateView(for: index) // Update the view based on the selected tab
+                                        }
+                                    Image(systemName: item.imageName)
+                                        .resizable()
+                                        .foregroundColor(Color(self.darkBackground))
+                                        .frame(width: self.iconSize, height: self.iconSize)
+                                        .opacity(item.opacity)
+                                }
+                                .offset(y: item.offset)
                             }
-                            .offset(y: self.tabItems.items[i].offset)
                         }
                     }
                 }
+                .edgesIgnoringSafeArea(.all)
             }
-            .edgesIgnoringSafeArea(.all)
+        }
+        
+        // Function to update the view based on the selected tab index
+        private func updateView(for index: Int) {
+            switch index {
+            case 0:
+                selectedView = AnyView(Entertainment())
+            case 1:
+                selectedView = AnyView(GroupsView())
+            case 2:
+                selectedView = AnyView(ContentView())
+            default:
+                selectedView = AnyView(GroupsView())
+            }
         }
     }
-}
+
 
 struct TabBar_Previews: PreviewProvider {
     static var previews: some View {
